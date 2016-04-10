f2699f5e15e2e7c83db96819723cd6a20a433538
Alok Singh <alokbeniwal@gmail.com>
2016-04-10 01:47:35 -0700

    Decided upon One Hot Encoding

--- src/fill-in-missing.py
+++ src/fill-in-missing.py
@@ -3,6 +3,7 @@
 
 import math
 import random
+import csv
 import os
 import sys
 import subprocess
@@ -14,4 +15,28 @@ import matplotlib as plt
 import sklearn
 import numpy as np
 
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.preprocessing import Imputer
 
+from ptpdb import set_trace
+
+# read in data
+# look at it
+census_data_file = open("./census_data/train_data.csv")
+census_data = csv.DictReader(census_data_file)
+print("census_data: {}".format(census_data))
+vectorized_dict = DictVectorizer(census_data)
+print("vectorized_dict: {}".format(vectorized_dict))
+
+
+categorical_vars = ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'native-country']
+
+# TODO put data into lists and put
+# X ->  [
+#           [  ]
+#                    ]
+# (n,m) where n is number of samples and m is number of features
+# TODO map th
+
+# Y -> [ {0,1} ] (n,1) or (n,)
+# TODO check if quant data is in string format and cast to float if needed
